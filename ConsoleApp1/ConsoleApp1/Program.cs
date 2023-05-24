83f79cc735a1471c1867053b5748ad5bd588f3d5
Dr.psychoo12 <maheshshrestha558@gmail.com>
2023-05-24 07:17:07 +0545

    class works

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int dayNumber;
+        Console.WriteLine("Enter a your number : ");
+        dayNumber = int.Parse(Console.ReadLine());
+        switch (dayNumber)
+        {
+            case 1:
+                Console.WriteLine("Sunday");
+                break;
+            case 2:
+                Console.WriteLine("Monday");
+                break;
+            case 3:
+                Console.WriteLine("Tuesday");
+                break;
+            case 4:
+                Console.WriteLine("Wednesday");
+                break;
+            case 5:
+                Console.WriteLine("Thursday");
+                break;
+            case 6:
+                Console.WriteLine("Friday");
+                break;
+            case 7:
+                Console.WriteLine("Saturday");
+                break;
+            default:
+                Console.WriteLine("Invalid day number");
+                break;
+        }
+    }
+}
---
+++ oddEven/oddEven/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace oddeven
+{
+    class oddeven
+    {
+        static void Main(string[] args)
+        {
+
+            int a, b, sum,c;
+            Console.WriteLine("Hello World!");
+
+            Console.WriteLine("Enter a first number :");
+            a = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter a second number :");
+            b=int.Parse(Console.ReadLine());
+
+            sum = a + b;
+            if (sum % 2 == 0) {
+                Console.WriteLine("sum of two number is even at first :" +sum );
+
+                c = sum - 1;
+                Console.WriteLine("changing into odd :" +c );
+
+            }
+            else
+            {
+                Console.WriteLine("sum of two number is odd at first :" + sum);
+
+                c = sum + 1;
+                Console.WriteLine("changing into even :" + c);
+
+
+            }
+
+
+
+
+
+        }
+    }
+}
